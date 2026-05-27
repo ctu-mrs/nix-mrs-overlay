94b2614cddf4a2dbb7356ed6a73205d41ce529d6
Tomas Baca <Tomas Baca>
2026-05-27 16:48:11 +0200

    updated flake

--- overlay.nix
+++ overlay.nix
@@ -18,11 +18,12 @@ in
     propagatedBuildInputs = [
       ros.std-msgs
       ros.geometry-msgs
+      ros.std-srvs
+      ros.sensor-msgs
     ];
   };
 
-
-  mrs_uav_testing = ros.buildRosPackage {
+  mrs_cmake = ros.buildRosPackage {
     pname = "mrs_cmake";
     version = "nix";
     src = inputs.src_mrs_cmake;
@@ -41,9 +42,21 @@ in
     buildType = "ament_cmake";
     nativeBuildInputs = [ ros.ament-cmake ];
     propagatedBuildInputs = [ 
+      # non-ros
+      final.eigen
+      final.yaml-cpp
+      final.boost
+
+      # our
       final.mrs_msgs
-      ros.sensor-msgs
       final.mrs_cmake
+
+      # official ros
+      ros.sensor-msgs
+      ros.nav-msgs
+      ros.ros-core
+      ros.tf2-geometry-msgs
+      ros.tf2-eigen
     ];
   };
 }
