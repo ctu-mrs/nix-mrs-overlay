04a76647cfc6c27a2b531de19386c4fb5b716824
Tomas Baca <Tomas Baca>
2026-06-06 14:33:47 +0200

    including test depends

--- overlay.nix
+++ overlay.nix
@@ -133,7 +133,7 @@ let
         # Strictly routed ROS dependencies to prevent ARG_MAX compiler crashes
         nativeBuildInputs = builtins.filter (x: x != null) (builtins.map resolveDep pkgData.buildtool_depends);
         buildInputs = builtins.filter (x: x != null) (builtins.map resolveDep pkgData.build_depends);
-        propagatedBuildInputs = builtins.filter (x: x != null) (builtins.map resolveDep (pkgData.exec_depends ++ (pkgData.build_export_depends or [])));
+        propagatedBuildInputs = builtins.filter (x: x != null) (builtins.map resolveDep (pkgData.exec_depends ++ (pkgData.build_export_depends or []) ++ (pkgData.test_depends or [])));
         checkInputs = builtins.filter (x: x != null) (builtins.map resolveDep pkgData.test_depends);
 
         doCheck = false;
